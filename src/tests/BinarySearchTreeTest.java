bdb173c2fe080350a885cce072adbf4bc1711b08
mwilchek <mwilchek@gmu.edu>
2017-07-09 22:28:13 -0400

    Updated BST test

--- src/tests/BinarySearchTreeTest.java
+++ src/tests/BinarySearchTreeTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import dataStructures.BinarySearchTree;
 import exceptions.Underflow;
+
 import java.util.Scanner;
 
 
@@ -31,10 +32,10 @@ public class BinarySearchTreeTest {
             System.out.println("contains (string) - 3");
             System.out.println("remove (string) - 4");
             System.out.println("get (string) - 5");
-            System.out.println("add (string) - 6");
+            System.out.println("add (string - 6");
             System.out.println("print (traversal order) - 7");
-            System.out.println("stop Testing - 8 \n");
-            System.out.print("Enter choice: ");
+            System.out.println("stop Testing - 8\n");
+            System.out.print("Enter choice:");
 
             if (kb.hasNextInt())
                 method4BST = kb.nextInt();
@@ -46,100 +47,97 @@ public class BinarySearchTreeTest {
             }
 
             System.out.println("");
+            switch (method4BST) {
+                case 1:  // isEmpty()
+                    System.out.println("isEmpty returns " + tree.isEmpty());
+                    break;
 
-            if (method4BST == 1)
-                System.out.println("isEmpty returns " + tree.isEmpty());
-            if (method4BST == 2) {
-                try {
-                    System.out.println("size2 returns " + tree.size2());
-                } catch (Underflow e) {
-                    System.out.println("Tree is empty.");
-                    e.printStackTrace();
-                }
-            }
-            if (method4BST == 3) {
-                System.out.print("Enter a string to search for: ");
-                target = kb.nextLine();
-                System.out.println("contains(" + target + ") returns: " + tree.contains(target));
-            }
+                case 2:  // size()
+                    System.out.println("size returns " + tree.size());
+                    break;
 
-            if (method4BST == 4) {
-                System.out.print("Enter a string to remove: ");
-                target = kb.nextLine();
-                System.out.println("remove(" + target + ") returns " + tree.remove(target));
-            }
+                case 3:  // contains()
+                    System.out.print("Enter a string to search for: ");
+                    target = kb.next();
+                    System.out.println("contains(" + target + ") returns: " + tree.contains(target));
+                    break;
 
-            if (method4BST == 5) {
-                System.out.print("Enter a string to get: ");
-                target = kb.nextLine();
-                System.out.println("get(" + target + ") returns " + tree.get(target));
-            }
+                case 4:  // remove()
+                    System.out.print("Enter a string to remove: ");
+                    target = kb.next();
+                    System.out.println("remove(" + target + ") returns " + tree.remove(target));
+                    break;
 
-            if (method4BST == 6) {
-                System.out.print("Enter a string to add: ");
-                element = kb.nextLine();
-                tree.add(element);
-            }
+                case 5:  // get()
+                    System.out.print("Enter a string to get: ");
+                    target = kb.next();
+                    System.out.println("get(" + target + ") returns " + tree.get(target));
+                    break;
 
-            if (method4BST == 7) {
-                System.out.println("Choose a traversal order to test by selecting the corresponding number:");
-                System.out.println("1: Pre-order");
-                System.out.println("2: In-order");
-                System.out.println("3: Post-order");
-                if (kb.hasNextInt())
-                    order4BST = kb.nextInt();
-                else {
-                    System.out.println("Error: you must enter an integer.");
-                    System.out.println("Exiting test.");
-                    return;
-                }
-                System.out.println("");
+                case 6:  // add()
+                    System.out.print("Enter a string to add: ");
+                    element = kb.next();
+                    tree.add(element);
+                    break;
 
-                switch (order4BST) {
-                    case 1:
-                        treeSize = tree.reset(BinarySearchTree.PREORDER);
-                        System.out.println("The tree in Pre-order is: ");
-                        for (int count = 1; count <= treeSize; count++) {
-                            element = tree.getNext(BinarySearchTree.PREORDER);
-                            System.out.println(element);
-                        }
-                        break;
-
-                    case 2:
-                        treeSize = tree.reset(BinarySearchTree.INORDER);
-                        System.out.println("The tree in In-order is: ");
-                        for (int count = 1; count <= treeSize; count++) {
-                            element = tree.getNext(BinarySearchTree.INORDER);
-                            System.out.println(element);
-                        }
-                        break;
-
-                    case 3:
-                        treeSize = tree.reset(BinarySearchTree.POSTORDER);
-                        System.out.println("The tree in Post-order is: ");
-                        for (int count = 1; count <= treeSize; count++) {
-                            element = tree.getNext(BinarySearchTree.POSTORDER);
-                            System.out.println(element);
-                        }
-                        break;
-
-                    default:
-                        System.out.println("Error in order choice. Exiting test.");
+                case 7:  // Order tree and print
+                    System.out.println("Choose a traversal order to test by selecting the corresponding number:");
+                    System.out.println("1: Pre-order");
+                    System.out.println("2: In-order");
+                    System.out.println("3: Post-order");
+                    if (kb.hasNextInt())
+                        order4BST = kb.nextInt();
+                    else {
+                        System.out.println("Error: you must enter an integer.");
+                        System.out.println("Exiting test.");
                         return;
-                }
-            }
+                    }
+                    System.out.println("");
 
-            if (method4BST == 8) {
-                continueTest = false;
-            }
+                    switch (order4BST) {
+                        case 1:
+                            treeSize = tree.reset(BinarySearchTree.PREORDER);
+                            System.out.println("The tree in Pre-order is: ");
+                            for (int count = 1; count <= treeSize; count++) {
+                                element = tree.getNext(BinarySearchTree.PREORDER);
+                                System.out.println(element);
+                            }
+                            break;
 
-            else {
-                System.out.println("Error in method selection. Terminating test.");
-            }
+                        case 2:
+                            treeSize = tree.reset(BinarySearchTree.INORDER);
+                            System.out.println("The tree in In-order is: ");
+                            for (int count = 1; count <= treeSize; count++) {
+                                element = tree.getNext(BinarySearchTree.INORDER);
+                                System.out.println(element);
+                            }
+                            break;
 
+                        case 3:
+                            treeSize = tree.reset(BinarySearchTree.POSTORDER);
+                            System.out.println("The tree in Post-order is: ");
+                            for (int count = 1; count <= treeSize; count++) {
+                                element = tree.getNext(BinarySearchTree.POSTORDER);
+                                System.out.println(element);
+                            }
+                            break;
+
+                        default:
+                            System.out.println("Error in order choice. Exiting test.");
+                            return;
+                    }
+                    break;
+
+                case 8:  // stop testing
+                    continueTest = false;
+                    break;
+
+                default:
+                    System.out.println("Error in method selection. Terminating test.");
+                    return;
+            }
         }
         System.out.println("Binary Search Tree test has completed.");
         kb.close();
     }
-
 }
